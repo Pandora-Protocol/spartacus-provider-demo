73a6d877cf6e8edaf37b659eab9542807cd63c02
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-09-12 06:56:46 +0300

    hcaptcha integration

--- config/config.js
+++ config/config.js
@@ -3,6 +3,12 @@ module.exports = {
     PORT: 8081,
 
     VERSION: "0.1",
-    
+
     PRIVATE_KEY: Buffer.from("", "hex"),
+
+    HCAPTCHA: {
+        SITE_KEY: "10000000-ffff-ffff-ffff-000000000001",
+        SECRET_KEY: "0x0000000000000000000000000000000000000000",
+    }
+
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-console.log("Spartacus Provider Demo started")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const config = require('../config/config')
-const express = require('express');
+
+console.log("Spartacus Provider Demo started")
+
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
     res.json({
@@ -11,9 +21,29 @@ app.get('/', function (req, res) {
     });
 })
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+app.get('/challenge/:hash', function (req, res) {
+
+    const {hash} = req.params;
+    if (!hash || hash.length !== 64 )
+        return res.render('error', {title: config.APP, error: 'Error hash'})
+
+    res.render('index', { title: config.APP, sitekey: config.HCAPTCHA.SITE_KEY, hash })
+})
+
+app.post('/sign', function (req, res){
+
+    const {hash, token} = req.body;
+
+    if (!hash || hash.length !== 64 )
+        return res.render('error', {title: config.APP, error: 'Error hash'})
+
+})
+
+const server = app.listen(8081, function () {
+
+    const host = server.address().address
+    const port = server.address().port
 
     console.log("Example app listening at http://%s:%s", host, port)
+
 })
